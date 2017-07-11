95b7a9b6b3a55eda258e233f0c10b8935f74b9e4
Kim Schulze <kimberlykayschulze@gmail.com>
2017-07-11 14:35:29 -0700

    switch between user 1 input and user 2 guess.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
-var convertSentence = function(phrase) {
-  console.log(phrase);
+var spliceSentence = function(phrase) {
   return phrase.split("");
+};
+
+var isAVowel = function(letter) {
+  if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u') {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+var noVowels = function(spliced) {
+  for (i = 0; i <= spliced.length; i++) {
+    if (isAVowel(spliced[i])) {
+      spliced[i] = "-";
+    }
+  }
+  return spliced;
+};
+
+var displayString = function(inputtedString) {
+  $(".playerOne").hide();
+  $(".challenge").show();
+  for (j = 0; j <= inputtedString.length; j++) {
+    $("p").append(inputtedString[j]);
+  }
 };
 
 $(function() {
@@ -8,6 +33,10 @@ $(function() {
     event.preventDefault();
     var sentence = $("#inputSentence").val();
 
-    var splicedSentence = convertSentence(sentence));
+    var splicedSentence = spliceSentence(sentence);
+    var noVowelsSentence = noVowels(splicedSentence);
+    console.log(noVowelsSentence);
+
+    displayString(noVowelsSentence);
   });
 });
