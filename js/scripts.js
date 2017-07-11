35a8cb2b794821c1cec8337c05cda839212d7056
Kim Schulze <kimberlykayschulze@gmail.com>
2017-07-11 13:46:15 -0700

    store inputted sentence into splicedSentence

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 var convertSentence = function(phrase) {
   console.log(phrase);
-  //return phrase.split("");
+  return phrase.split("");
 };
 
 $(function() {
   $("#inputValue").submit(function(event) {
     event.preventDefault();
     var sentence = $("#inputSentence").val();
-    convertSentence(sentence);
+
+    var splicedSentence = convertSentence(sentence));
   });
 });
